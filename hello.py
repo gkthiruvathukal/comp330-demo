6eb69c40273daafdbacd926a48221d32309b4fe0
George K. Thiruvathukal <gkt@cs.luc.edu>
2017-09-11 19:08:57 +0000

    added support for saying hello to many people

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 def say_hello(person):
    print ("Hello, %s" % person)
 
-say_hello("George")
-say_hello("Dave")
+people = ["George","Dave","Ada Lovelace", "Charles Babbage"]
+
+for person in people:
+   say_hello(person)
 
